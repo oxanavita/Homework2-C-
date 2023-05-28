52e59d4399ac0b11b179a4aca213e20ff78805d1
Oksana <oxana.vita91@mail.ru>
2023-05-28 10:01:59 +0300

    Задача 13

---
+++ homework06/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int Prompt(string vessage)
+{
+    Console.Write("Введите число: ");
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32 (value);
+    return result;
+}
+        
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber (int number)
+{
+    if (number < 100)
+    {   
+       Console.WriteLine("Третьей цифры нет");
+       return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber (number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
